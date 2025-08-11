ea4db413e8de3aa79bb0b627d7d848962aa18ab2
AvatanshuGupta <avatanshugupta@gmail.com>
2025-08-12 00:44:24 +0530

    schema rectified

--- schema/input_schema.py
+++ schema/input_schema.py
@@ -8,5 +8,5 @@ class crop_parameter(BaseModel):
     temp:Annotated[float,Field(...,title="Temperature",description="Enter the temperature of farm environment in degree celsius",gt=0,lt=60,examples=["20.978","19.977","30.76"])]
     humidity:Annotated[float,Field(...,title="Humidity",description="Enter the Humidity of farm environment in percent",gt=0,lt=100,examples=["82.00274423","80.15836264"])]
     ph:Annotated[float,Field(...,title="Ph",description="Enter the Ph of farm environment",gt=0,lt=14,examples=["6.3434","4.2439"])]
-    rainfall:Annotated[float,Field(...,title="rainfall",description="Enter the rainfall in cm",gt=0,lt=100,examples=["82.00274423","80.15836264"])]
+    rainfall:Annotated[float,Field(...,title="rainfall",description="Enter the rainfall in cm",gt=0,examples=["82.00274423","80.15836264"])]
 
